b901bb79aa3e84b2aeb19fdaa0ae9f0ea5172236
Sienna Williams <3.14sienna@gmail.com>
2023-02-18 19:25:28 +0000

    problem 4

--- open_space_publisher.py
+++ open_space_publisher.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import Float32
+from sensor_msgs.msg import LaserScan
 import math
 
 def callback(data):
     pub_d = rospy.Publisher('open_space/distance', Float32)
     pub_a = rospy.Publisher('open_space/angle', Float32)
-    r = data.data
+    ls = data
+    r = ls.ranges
 
     l = 0
     ang = None
@@ -14,7 +16,7 @@ def callback(data):
     for i, d in enumerate(r):
         if d > l:
             l = d
-            ang = (-1.0 * 2.0 / 3.0) * math.pi + float(i) * (math.pi * 1.0 / 300.0)
+            ang = ((-1.0 * 2.0 / 3.0) * math.pi) + (float(i) * (math.pi * 1.0 / 300.0))
 
     
 
@@ -27,7 +29,7 @@ def listener():
     #Initialize node
     rospy.init_node('open_space_publisher', anonymous = True)
 
-    rospy.Subscriber('fake_scan', Float32, callback)
+    rospy.Subscriber('fake_scan', LaserScan, callback)
 
     rospy.spin()
 
