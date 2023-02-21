9289a3b5bf9f49e431e79e2261e0baecdb23cb9c
Sienna Williams <3.14sienna@gmail.com>
2023-02-21 20:40:02 +0000

    problem 4+5

--- open_space_publisher.py
+++ open_space_publisher.py
@@ -2,11 +2,15 @@
 import rospy
 from std_msgs.msg import Float32
 from sensor_msgs.msg import LaserScan
+from ros_exercises.msg import OpenSpace
 import math
 
 def callback(data):
-    pub_d = rospy.Publisher('open_space/distance', Float32)
-    pub_a = rospy.Publisher('open_space/angle', Float32)
+    #pub_d = rospy.Publisher('open_space/distance', Float32)
+    #pub_a = rospy.Publisher('open_space/angle', Float32)
+    pub_m = rospy.Publisher('open_space', OpenSpace)
+    msg = OpenSpace()
+
     ls = data
     r = ls.ranges
 
@@ -19,19 +23,30 @@ def callback(data):
             ang = ((-1.0 * 2.0 / 3.0) * math.pi) + (float(i) * (math.pi * 1.0 / 300.0))
 
     
+    msg.angle = ang
+    msg.distance = l
 
-    rospy.loginfo(l)
-    rospy.loginfo(ang)
-    pub_d.publish(l)
-    pub_a.publish(ang)    
+    rospy.loginfo(msg)
+    pub_m.publish(msg)
+
+
+    #rospy.loginfo(l)
+    #rospy.loginfo(ang)
+    #pub_d.publish(l)
+    #pub_a.publish(ang)    
 
 def listener():
     #Initialize node
     rospy.init_node('open_space_publisher', anonymous = True)
 
-    rospy.Subscriber('fake_scan', LaserScan, callback)
+    rate = rospy.Rate(20) # 20hz
 
-    rospy.spin()
+    while not rospy.is_shutdown():
+        rospy.Subscriber('fake_scan', LaserScan, callback)
+
+        rospy.spin()
+
+        rate.sleep()
 
 
 if __name__ == '__main__':
